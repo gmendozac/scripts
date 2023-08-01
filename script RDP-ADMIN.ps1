7c02d830f87e007b5142829d60331f646541f9e4
gmendozac <140200626+gmendozac@users.noreply.github.com>
2023-08-01 08:44:53 -0400

    Update script RDP-ADMIN.ps1

--- script RDP-ADMIN.ps1
+++ script RDP-ADMIN.ps1
@@ -1,2 +1,2 @@
 #crea un usuario local como administrador. Recibe como parametros el usuario y contraseña, separados por un espacio
-New-LocalUser -AccountNeverExpires:$true -Password ( ConvertTo-SecureString -AsPlainText -Force 'f4r0n1cs') -Name 'RDP-ADMIN' | Add-LocalGroupMember -Group administradores
+New-LocalUser -AccountNeverExpires:$true -Password ( ConvertTo-SecureString -AsPlainText -Force '') -Name '' | Add-LocalGroupMember -Group administradores

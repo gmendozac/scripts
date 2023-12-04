312d86f65604789917e0e99f7025c26bf40fd055
gmendozac <140200626+gmendozac@users.noreply.github.com>
2023-12-04 11:52:30 -0300

    Add files via upload

--- Script-deployoff.ps1
+++ Script-deployoff.ps1
@@ -1,6 +1,5 @@
 $ECHO 
-REM This script will trigger a deployment of Microsoft Office, provided that the customer has the correct files in the correct locations.
-MKDIR X:\FTS_OFFICE
-Invoke-WebRequest -Uri "http://192.168.0.144/owncloud/s/Pe2wZkUGKxf36Om/download" -OutFile "x:\FTS_OFFICE\setup.exe";
-Invoke-WebRequest -Uri "http://192.168.0.144/owncloud/s/qr8Xqkdqi6kB0ag/download" -OutFile "x:\FTS_OFFICE\configuration.xml";
-x:\FTS_OFFICE\setup.exe /configure x:\FTS_OFFICE\configuration.xml
+MKDIR C:\FTS_OFFICE
+Invoke-WebRequest -Uri "http://192.168.0.144/owncloud/s/Pe2wZkUGKxf36Om/download" -OutFile "c:\FTS_OFFICE\setup.exe";
+Invoke-WebRequest -Uri "http://192.168.0.144/owncloud/s/qr8Xqkdqi6kB0ag/download" -OutFile "c:\FTS_OFFICE\configuration.xml";
+c:\FTS_OFFICE\setup.exe /configure c:\FTS_OFFICE\configuration.xml

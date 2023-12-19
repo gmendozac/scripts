e23b7e24ad9d16a958c4726e263ecff7507f4b0d
gmendozac <140200626+gmendozac@users.noreply.github.com>
2023-12-19 11:18:53 -0300

    Delete Script-activaoff.ps1

--- Script-activaoff.ps1
+++
@@ -1,4 +0,0 @@
-$ECHO 
-CD C:\FTS_OFFICE
-Invoke-WebRequest -Uri "http://192.168.0.144/owncloud/s/3c5vrbV0MLyja10/download" -OutFile "C:\FTS_OFFICE\ActivaOffice2019.bat";
-C:\FTS_OFFICE\ActivaOffice2019.bat

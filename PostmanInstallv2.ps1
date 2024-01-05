e653b9168c7b477f0cdc98e51b069660740601f3
gmendozac <140200626+gmendozac@users.noreply.github.com>
2024-01-05 10:04:38 -0300

    Add files via upload

---
+++ PostmanInstallv2.ps1
@@ -0,0 +1,4 @@
+﻿MKDIR C:\Postman64
+Invoke-WebRequest -Uri http://192.168.0.144/owncloud/s/G5oe7o6dHOLTLpR/download -OutFile "c:\Postman64\Postman-win64-Setup.exe";
+Start-Process "c:\Postman64\Postman-win64-Setup.exe" -Argumentlist '-s' -NoNewWindow -Wait
+Start-Sleep 10

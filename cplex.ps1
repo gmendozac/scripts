be331b617058c5d9185d5a10881d9b3f8bd12b8d
gmendozac <140200626+gmendozac@users.noreply.github.com>
2024-01-08 15:19:17 -0300

    Add files via upload

---
+++ cplex.ps1
@@ -0,0 +1,4 @@
+﻿MKDIR C:\CPLEX
+Invoke-WebRequest -Uri "http://192.168.0.144/owncloud/s/i2AaMnVE3gokoDG/download" -OutFile "C:\CPLEX\cplexresp.properties"
+Invoke-WebRequest -Uri "http://192.168.0.144/owncloud/s/LdEhxnrlX6LCOjj/download" -OutFile "C:\CPLEX\cplex_studio129.win-x86-64.exe"
+C:\CPLEX\cplex_studio129.win-x86-64.exe -f C:\CPLEX\cplexresp.properties

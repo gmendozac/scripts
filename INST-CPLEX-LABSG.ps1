7f1c3f0e09bda71e177b631c30ff78d2729fb123
gmendozac <140200626+gmendozac@users.noreply.github.com>
2024-01-11 15:00:55 -0300

    Add files via upload

---
+++ INST-CPLEX-LABSG.ps1
@@ -0,0 +1,4 @@
+﻿MKDIR C:\CPLEX
+Invoke-WebRequest -Uri "http://200.1.25.137:65000/owncloud/s/StKNsW6P3kdavQG/download" -OutFile "C:\CPLEX\cplexresp.properties"
+Invoke-WebRequest -Uri "http://200.1.25.137:65000/owncloud/s/BbBcqAEENcLES9z/download" -OutFile "C:\CPLEX\cplex_studio129.win-x86-64.exe"
+C:\CPLEX\cplex_studio129.win-x86-64.exe -f C:\CPLEX\cplexresp.properties
